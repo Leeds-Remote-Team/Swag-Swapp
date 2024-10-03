f6f843f67514d0db6edec54b4c5433e8a7dd7a46
zaw-tun <zaw.tun@hotmail.com>
2024-10-03 16:29:02 +0100

    added UserContext

--- SWAG-SWAPP/app/_layout.tsx
+++ SWAG-SWAPP/app/_layout.tsx
@@ -2,14 +2,18 @@ import { Stack } from "expo-router";
 import { useState, useContext, createContext } from "react";
 
 export const ImageContext = createContext(null);
+export const UserAccountContext = createContext(null);
 
 export default function RootLayout() {
   const [testImage, setTestImage] = useState("");
+  const [userAccount, setUserAccount] = useState("");
   return (
-    <ImageContext.Provider value={[testImage, setTestImage]}>
-      <Stack>
-        <Stack.Screen name="index" />
-      </Stack>
-    </ImageContext.Provider>
+    <UserAccountContext.Provider value={[userAccount, setUserAccount]}>
+      <ImageContext.Provider value={[testImage, setTestImage]}>
+        <Stack>
+          <Stack.Screen name="index" />
+        </Stack>
+      </ImageContext.Provider>
+    </UserAccountContext.Provider>
   );
 }
