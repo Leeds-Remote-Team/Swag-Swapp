3acc4b52e1296b178b83155fee06e55bca4bf7df
chris-wtly <christianwhitley19@outlook.com>
2024-10-04 15:04:57 +0100

    pushing context

--- SWAG-SWAPP/app/_layout.tsx
+++ SWAG-SWAPP/app/_layout.tsx
@@ -1,19 +1,20 @@
 import { Stack } from "expo-router";
 import { useState, useContext, createContext } from "react";
+import { useRef } from "react";
 
-export const ImageContext = createContext(null);
+export const ClothesContext = createContext(null);
 export const UserAccountContext = createContext(null);
 
 export default function RootLayout() {
-  const [testImage, setTestImage] = useState("");
+  const [clothesItems, setClothesItems] = useState({});
   const [userAccount, setUserAccount] = useState("");
   return (
     <UserAccountContext.Provider value={[userAccount, setUserAccount]}>
-      <ImageContext.Provider value={[testImage, setTestImage]}>
+      <ClothesContext.Provider value={[clothesItems, setClothesItems]}>
         <Stack>
           <Stack.Screen name="index" />
         </Stack>
-      </ImageContext.Provider>
+      </ClothesContext.Provider>
     </UserAccountContext.Provider>
   );
 }
