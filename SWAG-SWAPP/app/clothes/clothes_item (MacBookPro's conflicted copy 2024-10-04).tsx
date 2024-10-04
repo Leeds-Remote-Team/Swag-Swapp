43c3e275becd2bd027bbacd479f4f32caf0403ed
zaw-tun <zaw.tun@hotmail.com>
2024-10-04 13:52:28 +0100

    work in progress on single clothes items

--- SWAG-SWAPP/app/clothes/clothes_item (MacBookPro's conflicted copy 2024-10-04).tsx
+++ SWAG-SWAPP/app/clothes/clothes_item (MacBookPro's conflicted copy 2024-10-04).tsx
@@ -134,3 +134,121 @@ const styles = StyleSheet.create({
 });
 
 export default clothes_item;
+
+// import React from "react";
+// import {
+//   Button,
+//   Image,
+//   Text,
+//   TextInput,
+//   View,
+//   StyleSheet,
+//   TouchableOpacity,
+//   ScrollView,
+// } from "react-native";
+// import { useState, createContext, useContext } from "react";
+// import { Link } from "expo-router";
+// import { UserAccountContext } from "../_layout";
+// import { Header } from "../Header";
+
+// export default function clothes() {
+//   const userAccount = useContext(UserAccountContext);
+
+//   return (
+//     <View style={styles.itemImageContainer}>
+//       <Header />
+//       <Text style={styles.descriptionLabel}>Clothes Item</Text>
+//       <Image
+//         source={{ uri: "https://img.icons8.com/ios/100/000000/jumper.png" }}
+//         style={styles.itemImage}
+//       />
+
+//       <View style={styles.tagContainer}>
+//         {["Top Category", "Category", "Color"].map((tag, index) => (
+//           <View key={index} style={styles.tag}>
+//             <Button title={tag} />
+//           </View>
+//         ))}
+//       </View>
+
+//       <Text style={styles.descriptionLabel}>Description</Text>
+//       <Text style={styles.descriptionText}> random text description </Text>
+
+//       <Text style={styles.descriptionText}>Last Worn: 04/10/2024</Text>
+//       <Text style={styles.descriptionText}>Wear Frequency: 5</Text>
+
+//       <TouchableOpacity style={styles.wearTodayButton}>
+//         <Text style={styles.buttonText}>Wearing Today</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 20,
+//     color: "grey",
+//   },
+//   itemImageContainer: {
+//     alignItems: "center",
+//     marginVertical: 20,
+//     padding: 20,
+//     backgroundColor: "#fff",
+//     borderRadius: 20,
+//   },
+//   itemImage: {
+//     width: 200,
+//     height: 200,
+//     resizeMode: "contain",
+//     borderRadius: 10,
+//     marginBottom: 20,
+//   },
+//   wearTodayButton: {
+//     alignSelf: "center",
+//     backgroundColor: "#4CAF50",
+//     padding: 10,
+//   },
+//   buttonText: {
+//     fontWeight: "bold",
+//     color: "#fff",
+//   },
+//   tagContainer: {
+//     flexDirection: "row",
+//     justifyContent: "center",
+//     marginBottom: 20,
+//     flexWrap: "wrap",
+//     marginVertical: 10,
+//   },
+//   tag: {
+//     backgroundColor: "#ddd",
+//     paddingHorizontal: 15,
+//     paddingVertical: 5,
+//     borderRadius: 20,
+//     borderWidth: 1,
+//     borderColor: "#ccc",
+//     marginBottom: 10
+//   },
+//   descriptionLabel: {
+//     fontWeight: "bold",
+//     fontSize: 16,
+//     marginBottom: 10,
+//     color: "#333",
+//   },
+//   descriptionText: {
+//     fontSize: 16,
+//     color: "#555",
+//     marginBottom: 10,
+//     justifyContent: "space-between",
+//   },
+// });
+
+// // Get hold of the single clothe id which is item_id.
+// //useEffect to fetch the item through query database
+// //Extract the datas from our database and Ximilar Api to be able to design our page
+// //Design the page with the data.
+
+// //This page will get hold of the data from API and dispaly properly.
+// //2 Pages
+// //1 from the Ximilar API and
+// //2 from the main main page ---> when a user clicks an item in the page.
